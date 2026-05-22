aa313bb83bb0d22ebaeb713e5a09bedb8ea3d9e5
m01nikhil <mahlawat.nikhil01@gmail.com>
2026-05-22 14:38:19 +0000

    Conversion of datatypes

--- 01_basics/tests.js
+++ 01_basics/tests.js
@@ -1,7 +1,22 @@
-console.log("test")
 
-let score = "33"
+
+let score = "33abc"
 console.log(typeof score)
 
 let valueInNumber = Number(score)
+console.log(valueInNumber)
 console.log(typeof valueInNumber)
+
+let score1 = null
+console.log(typeof score1)
+
+let score2 = undefined
+console.log(typeof score2)
+
+let score3 = "3Abcdj"
+console.log(typeof score3)
+
+let mark = 43
+let BooleanNumber = Boolean(mark)
+console.log(BooleanNumber)
+console.log(typeof BooleanNumber)
