eb88485869d7b23e0570eeb3998545fbc7a00f3a
m01nikhil <mahlawat.nikhil01@gmail.com>
2026-05-28 16:01:38 +0000

    math and num

---
+++ conversionOperation.js
@@ -0,0 +1,16 @@
+let value = 67
+let value2 = -value
+//console.log(value2)
+
+//console.log(Math)
+//console.log(Math.abs(-8))
+//console.log(Math.round(4.5))
+//console.log(Math.ceil(2.4))
+
+console.log(Math.random())
+console.log(Math.random() * 10 + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
