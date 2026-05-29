63b664f6e90f23c135d5b7cfb01eb132a703274a
m01nikhil <mahlawat.nikhil01@gmail.com>
2026-05-29 15:19:14 +0000

    dates

---
+++ dates.js
@@ -0,0 +1,18 @@
+// let mydate = new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleDateString())
+
+let mycreateddate = new Date(2023, 0, 23)
+//console.log(mycreateddate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(mycreateddate.getTime())
+
+let newDate = new Date()
+console.log(new Date)
+console.log(newDate.getMonth() + 6)
+console.log(newDate.getDay())
+
