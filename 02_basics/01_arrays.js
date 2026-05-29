7479cb63486d95244fbeb193eb354bfe017041ab
m01nikhil <mahlawat.nikhil01@gmail.com>
2026-05-29 17:26:21 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+myArr = [0, 1, 2, 3, 4, 5, 6]
+
+//console.log(myArr[4])
+
+myArr.push(9)
+//console.log(myArr)
+
+myArr.pop(10)
+//console.log(myArr)
+
+// console.log(myArr.includes(9))
+
+
+// console.log(myArr)
+
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1)
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2)
