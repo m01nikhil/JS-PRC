f431665ab2c349080ee842010d3a19b08da0b1b3
m01nikhil <mahlawat.nikhil01@gmail.com>
2026-06-02 13:17:25 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,15 @@
+function addTwonumbers(number1, number2) {
+    return number1 + number2
+}
+let nums = addTwonumbers(456789, 7342895)
+// console.log("Result: ", nums)
+
+
+
+function loggedInmessage(username) {
+    return `${username} just logged in `
+}
+let mess = loggedInmessage("nikhil")
+// console.log(mess)
+
+
